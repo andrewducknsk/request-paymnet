3f952f0a7d16a40776b69f76d6baaba7adb35963
Andrew <andewkinev@yandex.ru>
2019-09-15 18:51:05 +0700

    push docs

---
+++ docs/precache-manifest.45525ebebee839ecfd39d51e929077f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a400648c3296e755cd539e8dd93129d3",
+    "url": "./static/media/burger.a400648c.jpg"
+  },
+  {
+    "revision": "4db12efdde574a06d8e80ebe9558f7e5",
+    "url": "./static/media/corn.4db12efd.jpg"
+  },
+  {
+    "revision": "408e36d605ebc719c577f2da572158df",
+    "url": "./static/media/pi.408e36d6.png"
+  },
+  {
+    "revision": "b0ff0ba087f471177814c95f334f7d8c",
+    "url": "./static/media/taco.b0ff0ba0.jpg"
+  },
+  {
+    "revision": "6dc6bd1bee5065e6fb37279cef2d24d4",
+    "url": "index.html"
+  },
+  {
+    "revision": "c4765a7b404e9da3f69b",
+    "url": "static/css/main.e19ba857.chunk.css"
+  },
+  {
+    "revision": "cb1fa735f4f268144482",
+    "url": "static/js/2.c695082c.chunk.js"
+  },
+  {
+    "revision": "c4765a7b404e9da3f69b",
+    "url": "static/js/main.44e2ef50.chunk.js"
+  },
+  {
+    "revision": "1aabc0bb0ef3afcd3294",
+    "url": "static/js/runtime~main.d586b8af.js"
+  }
+]);
